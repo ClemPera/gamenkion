4a8ddab3c8be3bd0910cbbb783f46899c46ef220
clemPera <clement.pera@gmail.com>
2025-09-17 20:03:43 -0400

    adding stuff

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use epd_waveshare::{epd2in13_v2::*, prelude::*};
 // };
 
 use embedded_graphics::{
-    image::ImageRaw, mono_font::{ascii::FONT_9X18_BOLD, mapping, DecorationDimensions, MonoFont, MonoTextStyle}, pixelcolor::BinaryColor, prelude::*, primitives::Rectangle, text::Text
+    image::ImageRaw, mono_font::{ascii::FONT_9X18_BOLD, mapping, DecorationDimensions, MonoFont, MonoTextStyle}, pixelcolor::BinaryColor, prelude::*, primitives::{Line, PrimitiveStyle, Rectangle}, text::Text
 };
 
 use esp_idf_svc::hal::{delay::Ets, gpio::{AnyIOPin, PinDriver}, spi::{config::Config, SpiDriverConfig}};
@@ -15,6 +15,7 @@ use esp_idf_svc::hal::prelude::Peripherals;
 
 use u8g2_fonts::{fonts::u8g2_font_ncenB24_tf, types::{FontColor, HorizontalAlignment, VerticalPosition}, Content, FontRenderer};
 
+use std::{thread, time};
 
 use esp_idf_dht;
 
@@ -62,12 +63,8 @@ fn main() {
   display.set_rotation(DisplayRotation::Rotate90);
   // display.fill_solid(&Rectangle::new(Point::new(0, 0), Size::new(30, 200)), Color::Black).unwrap();
   // Use embedded graphics for drawing a line
-  // let _ = Line::new(Point::new(0, 120), Point::new(0, 295))
-  // .into_styled(PrimitiveStyle::with_stroke(Color::White, 5))
-  // .draw(&mut display);
 
   let font = FontRenderer::new::<u8g2_font_ncenB24_tf>();
-  let text = "yo";
 
   font.render_aligned(
       format!("{}°C", dht.temperature).as_str(),
@@ -89,11 +86,41 @@ fn main() {
 
   //Draw text
   // let style = MonoTextStyle::new(, Color::Black);
-
   // Text::new(&format!("{}°C", dht.temperature), Point::new(20, 60), style).draw(&mut display).unwrap();
   // Text::new(&format!("{} %", dht.humidity), Point::new(150, 60), style).draw(&mut display).unwrap();
-  
+
+  let buf = display.buffer().len();
+  log::info!("buffer1: {buf:?}");
+
+  // display.fill_solid(&Rectangle::new(Point::new(10, 50), Size::new(80, 30)), Color::Black).unwrap();
+  // display.fill_solid(&Rectangle::new(Point::new(140, 50), Size::new(55, 30)), Color::Black).unwrap();
+
+
+  let buf = display.buffer().len();
+  log::info!("buffer2: {buf:?}");
+
+  //TOOD: this crash :(
   epd.update_and_display_frame(&mut device, &display.buffer(), &mut delay).unwrap();
+
+  // epd.sleep(&mut device, &mut delay).unwrap();
+
+  // thread::sleep(time::Duration::from_millis(1000));
   
-  epd.sleep(&mut device, &mut delay).unwrap();
+  // epd.wake_up(&mut device, &mut delay).unwrap();
+
+  // let buf = display.buffer().len();
+  // log::info!("buffer3: {buf:?}");
+  
+  // display.fill_solid(&Rectangle::new(Point::new(10, 50), Size::new(80, 30)), Color::Black).unwrap();
+
+  // let buf = display.buffer().len();
+  // log::info!("buffer4: {buf:?}");
+  
+  // epd.update_partial_frame(&mut device, &mut delay, &display.buffer(), 10, 50, 80, 30).unwrap();
+
+  // display.fill_solid(&Rectangle::new(Point::new(140, 50), Size::new(55, 30)), Color::Black).unwrap();
+  // epd.update_partial_frame(&mut device, &mut delay, &display.buffer(), 10, 50, 80, 30).unwrap();
+
+  // epd.display_frame(&mut device, &mut delay).unwrap();
+  // epd.sleep(&mut device, &mut delay).unwrap();
 }
